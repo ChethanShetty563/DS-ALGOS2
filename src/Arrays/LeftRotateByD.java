bdd7a4ea0e3580413873faf513779975bece38b1
ChethanShetty563 <chethan563@gmail.com>
2023-03-09 09:08:14 +0530

    Adding the reversal and naive appproach

--- src/Arrays/LeftRotateByD.java
+++ src/Arrays/LeftRotateByD.java
@@ -2,6 +2,9 @@ package Arrays;
 
 public class LeftRotateByD {
 
+//    Time Complexity O(n)
+//    Auxillary space O(d)
+//    Efficient approach
     public static void leftRotateByD(int arr[], int n, int d) {
 
         int temp[] = new int[d];
@@ -19,6 +22,53 @@ public class LeftRotateByD {
             arr[count++] = temp[i];
         }
     }
+
+//    NAive Approach
+static void lRotateOne(int arr[], int n)
+{
+    int temp = arr[0];
+
+    for(int i = 1; i < n; i++)
+    {
+        arr[i - 1] = arr[i];
+    }
+
+    arr[n - 1] = temp;
+}
+
+    static void leftRotate(int arr[], int d, int n)
+    {
+        for(int i = 0; i < d; i++)
+        {
+            lRotateOne(arr, n);
+        }
+    }
+
+//    Reversal method
+
+    static void leftRotateReverse(int arr[], int d, int n)
+    {
+        reverse(arr, 0, d - 1);
+
+        reverse(arr, d, n - 1);
+
+        reverse(arr, 0, n - 1);
+
+    }
+
+    static void reverse(int arr[], int low, int high)
+    {
+        while(low < high)
+        {
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+
+            low++;
+            high--;
+        }
+    }
+
 
     public static void main(String args[])
     {
